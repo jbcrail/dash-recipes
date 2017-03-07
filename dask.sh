f67aecb6c84076159a4b39bbc5ea760886259d58
Joseph Crail <jbcrail@gmail.com>
2017-03-06 20:30:42 -0500

    Bump Dask version

--- dask.sh
+++ dask.sh
@@ -4,7 +4,7 @@ SRC=https://github.com/dask/dask
 NAME=`basename ${SRC}`
 ENV=${NAME}-`cat /dev/urandom | base64 | head -c 4`
 DOCSET=Dask
-VERSION=0.13.0
+VERSION=0.14.0
 
 REPO_DIR=`pwd`/repos/${NAME}
 DOCSET_DIR=`pwd`/docsets/${NAME}
